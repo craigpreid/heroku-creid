96e31af73bd0e01eeff768a6d6d312aba8c1e2c4
craigpreid <cpreid@gmail.com>
2018-07-25 11:50:42 -0700

    commit 2

---
+++ config.py
@@ -0,0 +1,7 @@
+# Twitter API Keys
+consumer_key = "XfqYxitZjTGJYv1QLiTPnaQVe"
+consumer_secret = "pMTXh2ALilebb9JDH2w93d6sDH8gNCM2mnYvW4H4Uh6ZjPpNmi"
+access_token = "571191683-q3hxWkXriGXuAmzW0Xgnneq0JCrBRL19OVcaMS4P"
+access_token_secret = "iEs6bHhbuvigDhUlQwYMFWWdF7nwImyPMlLs7Xy2Fod6P"
+
+weather_api_key = "a75d5536ef5a39cbbecc4874227cc452"
