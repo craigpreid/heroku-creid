4abe4877fec30e75d40b6ab98491b01cac5c2a93
craigpreid <cpreid@gmail.com>
2018-07-26 19:55:07 -0700

    new add

--- ChatterBot.py
+++ ChatterBot.py
@@ -1,22 +1,3 @@
 # Dependencies
-import tweepy
-import time
-import json
-from config import consumer_key, consumer_secret, access_token, access_token_secret
-
-# Twitter API Keys
-consumer_key = consumer_key
-consumer_secret = consumer_secret
-access_token = access_token
-access_token_secret = access_token_secret
-
-# Setup Tweepy API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-
-# Create a function that tweets
-
-
 
 print("This is my Heroku app")
